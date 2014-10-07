883d868ec58c7b1f03e1e0ba3d4d16ce5d6503a2
Steve King, Jr <stevekx86@gmail.com>
2014-10-07 13:01:03 -0700

    Fixed menubar.

--- android/app/src/main/java/com/junipertech/kickback/Home.java
+++ android/app/src/main/java/com/junipertech/kickback/Home.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 
 public class Home extends Activity {
@@ -31,11 +32,13 @@ public class Home extends Activity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+        switch(item.getItemId()) {
+            case R.id.action_add_friends:
+                Toast.makeText(this, "Pressed \"Add Friends\".", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-        return super.onOptionsItemSelected(item);
     }
 
     public void kickback()
