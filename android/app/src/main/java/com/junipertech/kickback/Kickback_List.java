f8ecae039b7d5cefbdafb6f8cec7fe8841607129
Nathan Fegard <nfegard@asu.edu>
2014-10-07 03:17:31 -0700

    basic UI stuff for the android platform

--- android/app/src/main/java/com/junipertech/kickback/Kickback_List.java
+++ android/app/src/main/java/com/junipertech/kickback/Kickback_List.java
@@ -6,7 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class Kickback_List extends Activity {
+public class Kickback_List extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
