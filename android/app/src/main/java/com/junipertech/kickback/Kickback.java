b31b8eb2fe6da8a3d96ee93f3c6cc6477fe37229
Steve King, Jr <stevekx86@gmail.com>
2014-10-08 16:47:07 -0700

    Added basic Friend/Kickback models and started using Joda.

---
+++ android/app/src/main/java/com/junipertech/kickback/Kickback.java
@@ -0,0 +1,35 @@
+package com.junipertech.kickback;
+
+import org.joda.time.DateTime;
+import org.joda.time.Period;
+
+public class Kickback {
+
+    private DateTime start;
+    private DateTime stop;
+
+    public Kickback(DateTime start, DateTime stop) {
+        this.start = start;
+        this.stop = stop;
+    }
+
+    public Kickback(Kickback kickback) {
+        this.start = kickback.start;
+        this.stop = kickback.stop;
+    }
+
+    public DateTime getStart() {
+        return new Kickback(this).start;
+    }
+
+    public DateTime getStop () {
+        return new Kickback(this).stop;
+    }
+
+    public boolean overlaps(Kickback kickback) {
+        DateTime now = DateTime.now();
+        Period period = new Period(now, kickback.getStop());
+        return period.getHours() > 1;
+    }
+
+}
