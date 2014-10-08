09d99f3a2d0a3cf9b048daf41cc0ec09968fcd67
Nathan Fegard <nfegard@asu.edu>
2014-10-08 13:14:07 -0700

    added search layout for use with search actionbar items. (implemented in friends)

--- android/app/src/main/java/com/junipertech/kickback/Friends.java
+++ android/app/src/main/java/com/junipertech/kickback/Friends.java
@@ -1,6 +1,7 @@
 package com.junipertech.kickback;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -20,6 +21,8 @@ public class Friends extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.friends, menu);
+
+
         return true;
     }
 
@@ -28,10 +31,14 @@ public class Friends extends Activity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+        switch(item.getItemId()) {
+            case R.id.action_add_friend:
+
+                return true;
+            case R.id.action_search:
+
+                return true;
         }
-        return super.onOptionsItemSelected(item);
+        return true;
     }
 }
