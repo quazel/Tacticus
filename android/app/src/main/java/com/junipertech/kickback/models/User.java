d825b566e988c5372195231c18c632e55ca4ea0c
Steve King, Jr <stevekx86@gmail.com>
2014-10-10 10:12:53 -0700

    Deleted Fetch and implemented skeleton User model.

--- android/app/src/main/java/com/junipertech/kickback/models/User.java
+++ android/app/src/main/java/com/junipertech/kickback/models/User.java
@@ -1,4 +1,39 @@
 package com.junipertech.kickback.models;
 
 public class User {
+
+    private String username;
+    private String email;
+    private String phoneNumber;
+
+    public User(String username, String email, String phoneNumber) {
+        this.username = username;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
 }
