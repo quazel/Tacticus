eed2bba1869cccad29e1d999fffe51bee319a6be
Nathan Fegard <nfegard@asu.edu>
2014-10-11 19:42:49 -0700

    added basic non-functioning tabs to MyFriends activity

--- android/app/src/main/java/com/junipertech/kickback/activities/AddFriend.java
+++ android/app/src/main/java/com/junipertech/kickback/activities/AddFriend.java
@@ -1,37 +1,105 @@
 package com.junipertech.kickback.activities;
 
+import android.app.ActionBar;
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import com.junipertech.kickback.R;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
-public class AddFriend extends Activity {
+import com.junipertech.kickback.R;
+import com.junipertech.kickback.models.*;
+import com.junipertech.kickback.util.Globals;
+
+public class AddFriend extends FragmentActivity implements ActionBar.TabListener {
+
+    private static final String STATE_SELECTED_NAVIGATION_ITEM = "selected_navigation_item";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_friend);
         getActionBar().setDisplayHomeAsUpEnabled(true);
+
+        final ActionBar actionBar = getActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        // for each of the sections in the app, add a tab to the action bar.
+        actionBar.addTab(actionBar.newTab().setText(R.string.title_tab_add_friend)
+                .setTabListener(this));
+        actionBar.addTab(actionBar.newTab().setText(R.string.title_tab_friend_requests)
+                .setTabListener(this));
     }
 
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        // Restore the previously serialized current tab position.
+        if (savedInstanceState.containsKey(STATE_SELECTED_NAVIGATION_ITEM)) {
+            getActionBar().setSelectedNavigationItem(savedInstanceState.getInt(STATE_SELECTED_NAVIGATION_ITEM));
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        // Serialize the current tab position.
+        outState.putInt(STATE_SELECTED_NAVIGATION_ITEM, getActionBar()
+                .getSelectedNavigationIndex());
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.add_friend, menu);
+        getMenuInflater().inflate(R.menu.my_friends, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+    public void onTabSelected(ActionBar.Tab tab,
+                              FragmentTransaction fragmentTransaction) {
+        // When the given tab is selected, show the tab contents in the
+        // container view.
+        Fragment fragment = new DummySectionFragment();
+        Bundle args = new Bundle();
+        args.putInt(DummySectionFragment.ARG_SECTION_NUMBER,
+                tab.getPosition() + 1);
+        fragment.setArguments(args);
+        //getFragmentManager().beginTransaction()
+        //        .replace(R.id.container, fragment).commit();
     }
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab,
+                                FragmentTransaction fragmentTransaction) {
+    }
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab,
+                                FragmentTransaction fragmentTransaction) {
+    }
+
+
+    /**
+     * A dummy fragment representing a section of the app
+     */
+
+    public static class DummySectionFragment extends Fragment {
+        public static final String ARG_SECTION_NUMBER = "placeholder_text";
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            TextView textView = new TextView(getActivity());
+            textView.setGravity(Gravity.CENTER);
+            textView.setText(Integer.toString(getArguments().getInt(ARG_SECTION_NUMBER)));
+            return textView;
+        }
+    }
+
 }
