8fc79f72cf9c682c975f281f3765ffff95c7eb43
Steve King, Jr <stevekx86@gmail.com>
2014-10-12 19:11:38 -0700

    Splash screen is launch activity and alternates depending on whether user has registered.

--- android/app/src/main/java/com/junipertech/kickback/activities/SplashScreen.java
+++ android/app/src/main/java/com/junipertech/kickback/activities/SplashScreen.java
@@ -2,6 +2,7 @@ package com.junipertech.kickback.activities;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.Menu;
@@ -16,25 +17,27 @@ public class SplashScreen extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        SharedPreferences prefs = getSharedPreferences("KickbackPreferences", 0);
 
-        new Handler().postDelayed(new Runnable() {
+        if(prefs.contains("username")) {
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent i = new Intent(SplashScreen.this, Home.class);
+                    startActivity(i);
+                    finish();
+                }
+            }, SPLASH_TIME_OUT);
+        }
 
-            /*
-             * Showing splash screen with a timer. This will be useful when you
-             * want to show case your app logo / company
-             */
-
-            @Override
-            public void run() {
-                // This method will be executed once the timer is over
-                // Start your app main activity
-                Intent i = new Intent(SplashScreen.this, Home.class);
-                startActivity(i);
-
-                // close this activity
-                finish();
-            }
-        }, SPLASH_TIME_OUT);
+        else {
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    setContentView(R.layout.activity_splash_screen_login);
+                }
+            }, SPLASH_TIME_OUT);
+        }
     }
 
 
