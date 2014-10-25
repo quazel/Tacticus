08f300f10acd8eb6a577128aaad43ce2ce2903b2
Daniel Baird <JamesDanielBaird@gmail.com>
2014-10-24 17:42:50 -0700

    Uh
    Hmm

--- android/app/src/main/java/com/junipertech/kickback/activities/AddKickback.java
+++ android/app/src/main/java/com/junipertech/kickback/activities/AddKickback.java
@@ -19,6 +19,7 @@ import com.junipertech.kickback.models.Kickback;
 import com.junipertech.kickback.util.Globals;
 
 import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
 
 public class AddKickback extends Activity {
     Spinner month, day, year;
@@ -32,34 +33,35 @@ public class AddKickback extends Activity {
     private boolean start_am;
     private boolean end_am;
 
-    EditText locationInput;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_kickback);
         getActionBar().setDisplayHomeAsUpEnabled(true);
 
-        month = (Spinner) findViewById(R.id.months_input);
-        day = (Spinner) findViewById(R.id.day_input);
-        year = (Spinner) findViewById(R.id.year_input);
+        month = (Spinner)findViewById(R.id.months_input);
+        day = (Spinner)findViewById(R.id.day_input);
+        year = (Spinner)findViewById(R.id.year_input);
 
-        locationInput = (EditText) findViewById(R.id.activity_location);
+        currentTime = new DateTime();
+        inTwoWeeks = currentTime.plusDays(13); //ONLY ADDING 13 Because it is inclusive to current day
 
         start_am = true;
         end_am = true;
 
-        currentTime = new DateTime();
-        inTwoWeeks = currentTime;
-        inTwoWeeks = inTwoWeeks.plusDays(14);
+        setupSpinners();
+        //setSpinnerListeners();
 
-        setYearSpinner();
-        setMonthSpinner();
-        setDaySpinner();
 
-        //TODO Update spinners properly
 
-        setSpinnerListeners();
+        //updateDaySpinner();
+        //ArrayAdapter<String> dayAdapt = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, dayChoices);
+        //dayAdapt.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //day.setAdapter(dayAdapt);
+
+
+
+
 
     }
 
@@ -81,143 +83,102 @@ public class AddKickback extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
-    private void setYearSpinner() { //COMPLETE
-        String[] yearChoice;
-        if (currentTime.year() != inTwoWeeks.year()) {
-            yearChoice = new String[2];
-            yearChoice[0] = Integer.toString(currentTime.getYear());
-            yearChoice[1] = Integer.toString(inTwoWeeks.getYear());
-        } else {
-            yearChoice = new String[1];
-            yearChoice[0] = Integer.toString(currentTime.getYear());
+    private void updateDaySpinner(){ //TODO MAKE THIS WORK
+        int selectedMonth = Integer.parseInt(month.getSelectedItem().toString());
+
+        int currentDay = currentTime.getDayOfMonth();
+        int soonDay = inTwoWeeks.getDayOfMonth();
+
+        int from;
+        int to;
+
+        if(selectedMonth == currentTime.getMonthOfYear()){
+            if(currentDay<soonDay){
+                from = currentDay;
+                to = soonDay;
+
+                dayChoices = new String[14];
+                int index = 0;
+                for(int i=from;i<=to;i++){
+                    dayChoices[index] = Integer.toString(i);
+                    index++;
+                }
+            }else{
+                from = currentDay;
+                to = currentTime.dayOfMonth().getMaximumValue();
+
+                dayChoices = new String[1+(to-from)];
+                int index = 0;
+                for(int i=from;i<=to;i++){
+                    dayChoices[index] = Integer.toString(i);
+                    index++;
+                }
+            }
+        }else{
+            from = 1;
+            to = soonDay;
+
+            dayChoices = new String[1+(to-from)];
+            int index = 0;
+            for(int i=from;i<=to;i++){
+                dayChoices[index] = Integer.toString(i);
+                index++;
+            }
         }
 
-        ArrayAdapter<String> yearAdapt = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, yearChoice);
+    }
+
+    private void updateMonthSpinner(){
+        String currentMonth = Integer.toString(currentTime.getMonthOfYear());
+        String soonMonth = Integer.toString(inTwoWeeks.getMonthOfYear());
+        if(currentMonth.compareTo(soonMonth) == 0){ //If the years are the same
+            monthChoices = new String[1];
+            monthChoices[0] = currentMonth;
+            //Todo Set spinner unchangeable
+        }else{
+            monthChoices = new String[2];
+            monthChoices[0] = currentMonth;
+            monthChoices[1] = soonMonth;
+        }
+    }
+
+    private void setupSpinners(){
+        //First setup the year spinner
+        String currentYear = Integer.toString(currentTime.getYear());
+        String soonYear = Integer.toString(inTwoWeeks.getYear());
+        String[] yearChoices;
+        if(currentYear.compareTo(soonYear) == 0){ //If the years are the same
+            yearChoices = new String[1];
+            yearChoices[0] = currentYear;
+            //Todo Set spinner unchangeable
+        }else{
+            yearChoices = new String[2];
+            yearChoices[0] = currentYear;
+            yearChoices[1] = soonYear;
+        }
+        ArrayAdapter<String> yearAdapt = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, yearChoices);
         yearAdapt.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         year.setAdapter(yearAdapt);
-    }
 
-    private void setMonthSpinner() { //COMPLETE
-        ArrayAdapter<String> monthAdapt = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, monthChoices);
+        //Now setup the month spinner
+        //TODO ACCOUNT FOR YEAR ROLLOVER
+        updateMonthSpinner();
+        ArrayAdapter<String> monthAdapt = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, monthChoices);
         monthAdapt.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        year.setAdapter(monthAdapt);
-    }
+        month.setAdapter(monthAdapt);
 
-    private void setDaySpinner() { //COMPLETE
+        //Finally setup the day spinner
+        updateDaySpinner();
         ArrayAdapter<String> dayAdapt = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, dayChoices);
         dayAdapt.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         day.setAdapter(dayAdapt);
-    }
 
-
-
-    private void updateDaySpinner(){ //TODO REWORK SO THAT DAYS ARE ONLY WITHIN THE CURRENT DATE AND TWO WEEKS OUT. Based on month spinner listener
-
-        /*
-        int iMonth = month.getSelectedItemPosition()+1;
-        int iYear = Integer.parseInt(year.getSelectedItem().toString());
-        DateTime dt = new DateTime(iYear,iMonth,1,1,0);
-        int monthDays = dt.dayOfMonth().getMaximumValue();
-
-        dayChoices = new String[monthDays];
-        for(int i = 0; i<monthDays; i++){
-            dayChoices[i] = Integer.toString(i+1); //There has to be a better way of doing this
-        }
-        day.setSelection(currentTime.getDayOfMonth()-1);
-        */
-    }
-
-    private void updateMonthSpinner(){ //Given the year and the current/two weeks date what months can be loaded into the spinner
-        //TODO THIS
-
-        updateDaySpinner();
-    }
-
-    public void endTimeToggle(View view) { // COMPLETE
-        end_am = !((ToggleButton) view).isChecked();
-    }
-    public void startTimeToggle(View view) { //COMPLETE
-        start_am = !((ToggleButton) view).isChecked();
-    }
-
-    private DateTime getStart(){ //TODO DOUBLE CHECK THESE WITH NEW IMPLEMENTATION
-
-        /*
-        Spinner startTimeHours =(Spinner)findViewById(R.id.start_time_hours);
-        Spinner startTimeMinutes =(Spinner)findViewById(R.id.start_time_minutes);
-
-        int iMonth = month.getSelectedItemPosition()+1;
-        int iDay = Integer.parseInt(day.getSelectedItem().toString());
-        int iYear = Integer.parseInt(year.getSelectedItem().toString());
-
-        int iHours = Integer.parseInt(startTimeHours.getSelectedItem().toString());
-        if(!start_am){
-            iHours+=12;
-        }
-        int iMinutes = Integer.parseInt(startTimeMinutes.getSelectedItem().toString());
-
-        return new DateTime(iYear,iMonth,iDay,iHours,iMinutes);
-        */
-    }
-    private DateTime getEnd(){ //TODO DOUBLE CHECK THESE WITH NEW IMPLEMENTATION
-        /*
-        Spinner endTimeHours =(Spinner)findViewById(R.id.end_time_hours);
-        Spinner endTimeMinutes =(Spinner)findViewById(R.id.end_time_minutes);
-
-        int iMonth = month.getSelectedItemPosition()+1;
-        int iDay = Integer.parseInt(day.getSelectedItem().toString());
-        int iYear = Integer.parseInt(year.getSelectedItem().toString());
-
-        int iHours = Integer.parseInt(endTimeHours.getSelectedItem().toString());
-        if(!end_am){
-            iHours+=12;
-        }
-        int iMinutes = Integer.parseInt(endTimeMinutes.getSelectedItem().toString());
-
-        return new DateTime(iYear,iMonth,iDay,iHours,iMinutes);
-        */
-    }
-
-    public void addNewKickback(View view){
-
-        String location = locationInput.getText().toString();
-        DateTime start = getStart();
-        DateTime end = getEnd();
-
-        if(end.getHourOfDay() == 0 || end.getHourOfDay() == 24) {
-            end = end.plusDays(1);
-        }
-
-        if(end.isAfter(start)){ //TODO ADD MORE CHECKS FOR STUFF LIKE IF WE ARE TRYING TO ADD KICKBACKS WITH CONFLICTING DURATIONS
-            Kickback creationKickback = new Kickback(start,end,location);
-            Globals.addKickback(creationKickback);
-            //TODO ACTUALLY CREATE THE KICKBACK RIGHT NOW WE ARE TROWING KICKBACK AWAY
-
-            Context context = getApplicationContext();
-            CharSequence text = "Kickback added!";
-            int duration = Toast.LENGTH_SHORT;
-            Toast toast = Toast.makeText(context, text, duration);
-            toast.show();
-
-            Intent intent = new Intent(this, KickbacksSchedule.class);
-            startActivity(intent);
-            finish(); //Prevents user from coming back to this activity after submission
-        } else {
-            Context context = getApplicationContext();
-            CharSequence text = "The end time is not after the start time! Please try again!";
-            int duration = Toast.LENGTH_SHORT;
-            Toast toast = Toast.makeText(context, text, duration);
-            toast.show();
-        }
+        //Afterwards set the spinner listeners
+        setSpinnerListeners();
 
     }
 
-    public void cancelKickback(View view){
-        Intent intent = new Intent(this, KickbacksSchedule.class);
-        startActivity(intent);
-    }
-
-    public void setSpinnerListeners(){
+    private void setSpinnerListeners(){
         month.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
                 updateDaySpinner(); //Load Day Spinner on month change
@@ -227,17 +188,60 @@ public class AddKickback extends Activity {
                 //Do Nothing
             }
         });
-
         year.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                updateMonthSpinner(); //Load month spinner on year change
+                updateMonthSpinner(); //Load Day Spinner on month change
+
             }
 
             public void onNothingSelected(AdapterView<?> adapterView) {
                 //Do Nothing
             }
         });
+    }
+
+    public void endTimeToggle(View view) {
+        end_am = !((ToggleButton) view).isChecked();
+    }
+    public void startTimeToggle(View view) {
+        start_am = !((ToggleButton) view).isChecked();
+    }
+
+    private DateTime getStart(){
+        return new DateTime();
+    }
+    private DateTime getEnd(){
+        return new DateTime();
+    }
+
+    public void addNewKickback(View view){
+
+        EditText locationInfo = (EditText) findViewById(R.id.activity_location);
+        String location = locationInfo.getText().toString();
+        DateTime start = getStart();
+        DateTime end = getEnd();
+
+        if(end.getHourOfDay() == 0 || end.getHourOfDay() == 24) { //TODO CHECK THIS
+            end = end.plusDays(1);
+        }
+
+        //Check if it is a valid kickback
+        //Start is before end time
+        //No overlap with other kickbacks
 
     }
 
+    public void cancelKickback(View view){
+        Intent intent = new Intent(this, KickbacksSchedule.class);
+        startActivity(intent);
+    }
+
 }
+ 
+/*
+Context context = getApplicationContext();
+CharSequence text = "TEXT";
+int duration = Toast.LENGTH_SHORT;
+Toast toast = Toast.makeText(context, text, duration);
+toast.show();
+*/
