086050ce337bbdfdf0d623fd72ce4de2f6a18709
Daniel Baird <JamesDanielBaird@gmail.com>
2014-10-24 18:25:07 -0700

    Rollover Fixes
    chip

--- android/app/src/main/java/com/junipertech/kickback/activities/AddKickback.java
+++ android/app/src/main/java/com/junipertech/kickback/activities/AddKickback.java
@@ -46,8 +46,15 @@ public class AddKickback extends Activity {
         day = (Spinner)findViewById(R.id.day_input);
         year = (Spinner)findViewById(R.id.year_input);
 
+        //currentTime = new DateTime(2014,12,25,2,2); REMOVE FOR END OF YEAR ROLLOVER TESTING ONLY
         currentTime = new DateTime();
         inTwoWeeks = currentTime.plusDays(13); //ONLY ADDING 13 Because it is inclusive to current day
+
+        Context context = getApplicationContext();
+        CharSequence text = inTwoWeeks.getMonthOfYear()+" "+inTwoWeeks.getYear();
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
 
         start_am = true;
         end_am = true;
@@ -124,32 +131,52 @@ public class AddKickback extends Activity {
     }
 
     private void updateMonthSpinner(){
-        String currentMonth = Integer.toString(currentTime.getMonthOfYear());
-        String soonMonth = Integer.toString(inTwoWeeks.getMonthOfYear());
-        if(currentMonth.compareTo(soonMonth) == 0){ //If the years are the same
+        int currentMonth = currentTime.getMonthOfYear();
+        int soonMonth = inTwoWeeks.getMonthOfYear();
+
+        if(currentMonth == soonMonth){ //If the years are the same
             monthChoices = new String[1];
-            monthChoices[0] = currentMonth;
+            monthChoices[0] = Integer.toString(currentMonth);
             //Todo Set spinner unchangeable
         }else{
-            monthChoices = new String[2];
-            monthChoices[0] = currentMonth;
-            monthChoices[1] = soonMonth;
+            int yearSelected = Integer.parseInt(year.getSelectedItem().toString());
+            if(yearSelected == currentTime.getYear() && yearSelected == inTwoWeeks.getYear()){
+                monthChoices = new String[2];
+                monthChoices[0] = Integer.toString(currentMonth);
+                monthChoices[1] = Integer.toString(soonMonth);
+            }else{
+                monthChoices = new String[1];
+                if(yearSelected == currentTime.getYear()){
+                    monthChoices[0] = Integer.toString(currentMonth);
+                }else{ //Its the two weeks time
+                    monthChoices[0] = Integer.toString(soonMonth);
+                }
+            }
+
         }
+
+        monthAdapt = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, monthChoices);
+        monthAdapt.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        month.setAdapter(monthAdapt);
     }
 
     private void setupSpinners(){
         //First setup the year spinner
-        String currentYear = Integer.toString(currentTime.getYear());
-        String soonYear = Integer.toString(inTwoWeeks.getYear());
+        //String currentYear = Integer.toString(currentTime.getYear());
+        //String soonYear = Integer.toString(inTwoWeeks.getYear());
+
+        int currentYear = currentTime.getYear();
+        int soonYear = inTwoWeeks.getYear();
+
         String[] yearChoices;
-        if(currentYear.compareTo(soonYear) == 0){ //If the years are the same
+        if(currentYear == soonYear){ //If the years are the same
             yearChoices = new String[1];
-            yearChoices[0] = currentYear;
+            yearChoices[0] = Integer.toString(currentYear);
             //Todo Set spinner unchangeable
         }else{
             yearChoices = new String[2];
-            yearChoices[0] = currentYear;
-            yearChoices[1] = soonYear;
+            yearChoices[0] = Integer.toString(currentYear);
+            yearChoices[1] = Integer.toString(soonYear);
         }
         ArrayAdapter<String> yearAdapt = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, yearChoices);
         yearAdapt.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -158,9 +185,6 @@ public class AddKickback extends Activity {
         //Now setup the month spinner
         //TODO ACCOUNT FOR YEAR ROLLOVER
         updateMonthSpinner();
-        monthAdapt = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, monthChoices);
-        monthAdapt.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        month.setAdapter(monthAdapt);
 
         //Finally setup the day spinner
         updateDaySpinner();
@@ -174,7 +198,6 @@ public class AddKickback extends Activity {
         month.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
                 updateDaySpinner(); //Load Day Spinner on month change
-
             }
 
             public void onNothingSelected(AdapterView<?> adapterView) {
@@ -184,7 +207,6 @@ public class AddKickback extends Activity {
         year.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
                 updateMonthSpinner(); //Load Day Spinner on month change
-
             }
 
             public void onNothingSelected(AdapterView<?> adapterView) {
