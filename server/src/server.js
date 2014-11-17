023f0774968976396687f4948e4107f01437ea9f
Steve King, Jr <stevekx86@gmail.com>
2014-11-16 22:31:41 -0700

    Fixed packet parsing and added stub methods for AES.

--- server/src/server.js
+++ server/src/server.js
@@ -2,11 +2,40 @@ var net = require('net');
 
 net.createServer(function (socket) {
     socket.on('data', function (data) {
-        console.log('burgle');
+        var message = data.toString('utf8');
+        message = decryptSymetricMessage(message);
+        message = removePadding(message);
+        var packet = JSON.parse(message);
+    });
+    socket.on('end', function (data) {
+        console.log('User ended connection.');
+    });
+    socket.on('close', function (data) {
+        console.log('User closed connection');
     });
 }).listen(8000, '0.0.0.0');
 
-/* Encryption code for AES
+/*
+   Removes null and carriage return padding characters from string
+   Ensures that we get a string that can be parsed from JSON
+*/
+function removePadding (message) {
+    var re = /[\0\u0013]/g;
+    return message.replace(re, "");
+}
+
+// TODO: Implement AES decryption.
+function decryptSymetricMessage (message) {
+    return message;
+}
+
+// TODO: Implement AES encryption.
+function encryptSymetricMessage (message) {
+    return message;
+}
+
+/*
+Encryption code for AES
 crypto = require "crypto"
 var iv = new Buffer('asdfasdfasdfasdf')
 var key = new Buffer('asdfasdfasdfasdfasdfasdfasdfasdf')
