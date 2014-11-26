b018eacabf3077ba20c957f2e4391d34f07f3c18
Steve King, Jr <stevekx86@gmail.com>
2014-11-25 19:09:29 -0700

    Implemented KeepAlive packet on server side.

--- server/src/server.js
+++ server/src/server.js
@@ -6,7 +6,20 @@ net.createServer(function (socket) {
         message = decryptSymetricMessage(message);
         message = removePadding(message);
         var packet = JSON.parse(message);
-        // TODO: stuff with packet
+        switch (packet.packet_type) {
+            case 0:
+                console.log('Hello client!');
+                var responsePacket = Packet0KeepAlive();
+                var b = new Buffer(4);
+                var responseBody = JSON.stringify(responsePacket);
+                b.writeInt32BE(responseBody.length, 0);
+                socket.write(b);
+                socket.write(responseBody, 'utf8');
+                break;
+            default:
+                console.log("wut");
+                break;
+        }
     });
     socket.on('end', function (data) {
         console.log('User ended connection.');
@@ -16,9 +29,14 @@ net.createServer(function (socket) {
     });
 }).listen(8000, '0.0.0.0');
 
+
+function Packet0KeepAlive() {
+    return {type: 1, timestamp: Math.floor(new Date() / 1000), contents: "Pong!"};
+}
+
 /*
-   Removes null padding characters from string
-   Ensures that we get a string that can be parsed from JSON
+    Removes null padding characters from string
+    Ensures that we get a string that can be parsed from JSON
 */
 function removePadding (message) {
     var re = /[\0]/g;
@@ -36,11 +54,11 @@ function encryptSymetricMessage (message) {
 }
 
 /*
-Encryption code for AES
-crypto = require "crypto"
-var iv = new Buffer('asdfasdfasdfasdf')
-var key = new Buffer('asdfasdfasdfasdfasdfasdfasdfasdf')
-var cipher = crypto.createCipheriv('aes-256-cbc', key, iv);
-cipher.update(new Buffer("mystring"));
-var enc = cipher.final('base64');
+    Encryption code for AES
+    crypto = require "crypto"
+    var iv = new Buffer('asdfasdfasdfasdf')
+    var key = new Buffer('asdfasdfasdfasdfasdfasdfasdfasdf')
+    var cipher = crypto.createCipheriv('aes-256-cbc', key, iv);
+    cipher.update(new Buffer("mystring"));
+    var enc = cipher.final('base64');
 */
