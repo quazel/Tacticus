89db77c278376879daa22306b40039be823ba4a4
Steve King, Jr <stevekx86@gmail.com>
2014-11-26 19:51:30 -0700

    Stub methods and packet definitions.

--- server/src/packets.js
+++ server/src/packets.js
@@ -1,7 +1,61 @@
 module.exports = {
 
     Packet0KeepAlive: function () {
-        return {type: 1, timestamp: Math.floor(new Date() / 1000), contents: "Pong!"};
+        return {
+            packet_type: 0,
+            timestamp: Math.floor(new Date() / 1000),
+            contents: "Pong!"
+        };
+    },
+
+    Packet1Login: function () {
+        return {
+             packet_type: 1,
+             timestamp: Math.floor(new Date() / 1000),
+             contents: {
+                 token: "blurp"
+             }
+         };
+    },
+
+    Packet2UpdateStatus: function (new_status) {
+        return {
+            packet_type: 2,
+            timestamp: Math.floor(new Date() / 1000),
+            contents: {
+                status: new_status
+            }
+        };
+    },
+
+    Packet3FetchFriends: function () {
+        return {
+            packet_type: 3,
+            timestamp: Math.floor(new Date() / 1000),
+            contents: {
+
+            }
+        };
+    },
+
+    Packet4AddScheduleItem: function () {
+        return {
+            packet_type: 4,
+            timestamp: Math.floor(new Date() / 1000),
+            contents: {
+
+            }
+        };
+    },
+
+    Packet5UpdateScheduleItem: function () {
+        return {
+            packet_type: 5,
+            timestamp: Math.floor(new Date() / 1000),
+            contents: {
+
+            }
+        };
     }
 
 };
