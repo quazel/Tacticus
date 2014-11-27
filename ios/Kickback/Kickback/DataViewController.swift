5a0c7f56ea4fb2ddd8d95bcef130b4cfbecbbb35
Steve King, Jr <stevekx86@gmail.com>
2014-11-26 18:56:06 -0700

    Created Appcode project for iOS client.

---
+++ ios/Kickback/Kickback/DataViewController.swift
@@ -0,0 +1,47 @@
+//
+//  DataViewController.swift
+//  Kickback
+//
+//  Created by Steve King on 11/26/14.
+//  Copyright (c) 2014 Bramble. All rights reserved.
+//
+
+
+import UIKit
+
+
+class DataViewController: UIViewController {
+
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: AnyObject?
+
+
+
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+    }
+
+
+    override func viewWillAppear(animated: Bool) {
+    super.viewWillAppear(animated)
+    if let obj: AnyObject = dataObject {
+        self.dataLabel!.text = obj.description
+    } else {
+        self.dataLabel!.text = ""
+    }
+
+    }
+
+
+
+
+}
