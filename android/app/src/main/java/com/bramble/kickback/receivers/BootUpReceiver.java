1a9034cb95195ccd08abf11fc0bd89093280c4de
Nathan Fegard <nfegard@asu.edu>
2015-01-23 00:02:47 -0700

    added kickback service and made it start on boot

--- android/app/src/main/java/com/bramble/kickback/receivers/BootUpReceiver.java
+++ android/app/src/main/java/com/bramble/kickback/receivers/BootUpReceiver.java
@@ -1,16 +1,17 @@
 package com.bramble.kickback.receivers;
 
-
-        import android.content.BroadcastReceiver;
+import android.content.BroadcastReceiver;
         import android.content.Context;
         import android.content.Intent;
+
+        import com.bramble.kickback.service.KickbackService;
 
 public class BootUpReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        //Intent myIntent = new Intent(context, Notififier.class);
-        //context.startService(myIntent);
+        Intent myIntent = new Intent(context, KickbackService.class);
+        context.startService(myIntent);
     }
 
 }
