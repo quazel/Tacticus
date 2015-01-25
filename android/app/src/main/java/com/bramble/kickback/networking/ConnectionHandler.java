f5c4ba586d47ce507577638b4045bee6cc5396c0
Steve King, Jr <stevekx86@gmail.com>
2015-01-24 19:20:29 -0700

    Added some networking methods.

--- android/app/src/main/java/com/bramble/kickback/networking/ConnectionHandler.java
+++ android/app/src/main/java/com/bramble/kickback/networking/ConnectionHandler.java
@@ -13,10 +13,11 @@ import javax.net.ssl.HttpsURLConnection;
 public class ConnectionHandler {
 
     private final String serverAddress = "https://api.kickback.stevex86.com/";
-    private final int port = 80;
-    private final String baseURL = serverAddress + (port != 80 ? ":" + port : "");
+    private final int port = 443;
+    private final String baseURL = serverAddress + (port != 443 ? ":" + port : "");
     private final String userURL = baseURL + "user/";
     private final String loginURL = baseURL + "login";
+    private final String registerURL = baseURL + "register";
     private final String kickbackURL = baseURL + "kickback/";
     private final String analyticsURL = baseURL + "analytics/";
 
@@ -51,7 +52,85 @@ public class ConnectionHandler {
             return response.toString();
         }
         else {
-            throw new IOException();
+            throw new IOException("Received bad response from server.");
+        }
+    }
+
+    public String register(String username, String password, String email, String phoneNumber,
+                           String name, String birthdate, String sex) throws IOException
+    {
+        URL requestURL = new URL(registerURL);
+        HttpsURLConnection connection = (HttpsURLConnection) requestURL.openConnection();
+        connection.setDoInput(true);
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+        connection.setRequestProperty("charset", "utf-8");
+        String urlParameters = "";
+        urlParameters += "username=" + username;
+        urlParameters += "&password=" + password;
+        urlParameters += "&email=" + email;
+        urlParameters += "&phone_number=" + phoneNumber;
+        urlParameters += "&name=" + name;
+        urlParameters += "&birthdate=" + birthdate;
+        urlParameters += "&sex=" + sex;
+        connection.setRequestProperty("Content-Length", "" + urlParameters.getBytes().length);
+        connection.setUseCaches (false);
+        OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream());
+        writer.write(urlParameters);
+        writer.flush();
+        writer.close();
+
+        int responseCode = connection.getResponseCode();
+        if (responseCode == 200 || responseCode == 401) {
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+
+            while((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+            Log.d("ConnectionHandler", response.toString());
+            return response.toString();
+        }
+        else {
+            throw new IOException("Received bad response from server.");
+        }
+    }
+
+    public String checkCredentials(String username, String email, String phoneNumber) throws IOException {
+        URL requestURL = new URL(registerURL);
+        HttpsURLConnection connection = (HttpsURLConnection) requestURL.openConnection();
+        connection.setDoInput(true);
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+        connection.setRequestProperty("charset", "utf-8");
+        String urlParameters = "";
+        urlParameters += "username=" + username;
+        urlParameters += "&email=" + email;
+        urlParameters += "&phone_number=" + phoneNumber;
+        connection.setRequestProperty("Content-Length", "" + urlParameters.getBytes().length);
+        connection.setUseCaches (false);
+        OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream());
+        writer.write(urlParameters);
+        writer.flush();
+        writer.close();
+
+        int responseCode = connection.getResponseCode();
+        if (responseCode == 200 || responseCode == 401) {
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+
+            while((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+            Log.d("ConnectionHandler", response.toString());
+            return response.toString();
+        }
+        else {
+            throw new IOException("Received bad response from server.");
         }
     }
 
