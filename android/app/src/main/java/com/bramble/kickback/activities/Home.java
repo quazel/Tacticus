45ce221681e4e087f325079101dcdd7a9be8bc25
Nathan Fegard <nfegard@asu.edu>
2015-01-28 17:44:28 -0700

    started home rework

--- android/app/src/main/java/com/bramble/kickback/activities/Home.java
+++ android/app/src/main/java/com/bramble/kickback/activities/Home.java
@@ -3,6 +3,8 @@ package com.bramble.kickback.activities;
 import android.app.ActionBar;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,6 +14,9 @@ import android.view.View;
 import android.widget.Button;
 
 import com.bramble.kickback.R;
+import com.bramble.kickback.fragments.KickbackFragment;
+import com.bramble.kickback.fragments.KickbackList;
+import com.bramble.kickback.fragments.LoadingBar;
 import com.bramble.kickback.util.Globals;
 
 
@@ -19,17 +24,27 @@ public class Home extends Activity {
 
     private Button theButton;
 
+    private FragmentManager fm;
+    private FragmentTransaction ft;
+    private KickbackList kickbackList;
+    private KickbackFragment kickbackFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (Globals.isOnline) {
-            setContentView(R.layout.activity_home_online);
-        }
-        else {
-            setContentView(R.layout.activity_home);
-        }
+        setContentView(R.layout.activity_home);
+
+        fm = getFragmentManager();
+        kickbackFragment = new KickbackFragment();
+        kickbackList = new KickbackList();
+
         ActionBar actionbar = getActionBar();
         theButton =(Button)findViewById(R.id.kickbackButton);
+
+        ft = getFragmentManager().beginTransaction();
+        //ft.setCustomAnimations(android.R.animator, android.R.animator.fade_out);
+        ft.add(R.id.home_container, kickbackFragment);
+        ft.commit();
     }
 
 
@@ -72,15 +87,17 @@ public class Home extends Activity {
     }*/
 
     public void onKickbackPressed(View view) {
-
-            Globals.goOnline();
-            setContentView(R.layout.activity_home_online);
-
+        ft = getFragmentManager().beginTransaction();
+        //ft.setCustomAnimations(android.R.animator.fade_in, android.R.animator.fade_out);
+        ft.replace(R.id.home_container, kickbackList);
+        ft.commit();
     }
 
     public void onBusyPressed(View view) {
-        Globals.goOffline();
-        setContentView(R.layout.activity_home);
+        ft = getFragmentManager().beginTransaction();
+        //ft.setCustomAnimations(android.R.animator.fade_in, android.R.animator.fade_out);
+        ft.replace(R.id.home_container, kickbackFragment);
+        ft.commit();
     }
 
     public void onBackPressed() {
---
+++ android/app/src/main/java/com/bramble/kickback/fragments/KickbackFragment.java
@@ -0,0 +1,19 @@
+package com.bramble.kickback.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bramble.kickback.R;
+
+public class KickbackFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view =  inflater.inflate(R.layout.fragment_kickback,
+                container, false);
+        return view;
+    }
+}
