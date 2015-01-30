f3b49b754f676f5532f832e5986aa6ef9ccc3ecd
Nathan Fegard <nfegard@asu.edu>
2015-01-30 16:57:34 -0700

    those fragments be nested homie

--- android/app/src/main/java/com/bramble/kickback/fragments/HomeFragment.java
+++ android/app/src/main/java/com/bramble/kickback/fragments/HomeFragment.java
@@ -1,6 +1,8 @@
 package com.bramble.kickback.fragments;
 
 import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,10 +11,35 @@ import android.view.ViewGroup;
 import com.bramble.kickback.R;
 
 public class HomeFragment extends Fragment {
+
+    public OnlineFragment onlineFragment;
+    public OfflineFragment offlineFragment;
+    public FragmentManager homeManager;
+    public FragmentTransaction homeTransaction;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view =  inflater.inflate(R.layout.fragment_home, container, false);
+
+        offlineFragment = new OfflineFragment();
+        onlineFragment = new OnlineFragment();
+        homeManager = getFragmentManager();
+        homeTransaction = homeManager.beginTransaction();
+        homeTransaction.add(R.id.home_container, offlineFragment);
+        homeTransaction.commit();
+
         return view;
+    }
+
+    public void goOnline() {
+        homeTransaction = homeManager.beginTransaction();
+        homeTransaction.replace(R.id.home_container, onlineFragment);
+        homeTransaction.commit();
+    }
+
+    public void goOffline() {
+        homeTransaction = homeManager.beginTransaction();
+        homeTransaction.replace(R.id.home_container, offlineFragment);
+        homeTransaction.commit();
     }
 }
