d6d4bbc1954a74b99f5b054fa15579cbfbeea81f
Steve King, Jr <stevekx86@gmail.com>
2015-02-01 03:44:28 -0700

    Derpy looking tile system.

--- android/app/src/main/java/com/bramble/kickback/fragments/OnlineFragment.java
+++ android/app/src/main/java/com/bramble/kickback/fragments/OnlineFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
 
 import com.bramble.kickback.R;
 import com.bramble.kickback.adapter.FriendsStickyAdapter;
@@ -22,12 +24,17 @@ public class OnlineFragment extends Fragment {
     private ArrayList<Friend> onlineFriends;
     private StickyListHeadersListView stickyList;
     private OnlineFriendsStickyAdapter onlineFriendsStickyAdapter;
+    private GridView gridView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view =  inflater.inflate(R.layout.fragment_online, container, false);
 
+        gridView = (GridView) view.findViewById(R.id.friendGrid);
+        gridView.setAdapter(new ArrayAdapter<Friend>(view.getContext(), R.layout.online_tile_item, R.id.nickname_place, User.getUser().getOnlineFriends()));
+
+        /*
         onlineFriends = User.getUser().getOnlineFriends();
 
         stickyList = (StickyListHeadersListView) view.findViewById(R.id.online_list);
@@ -35,7 +42,7 @@ public class OnlineFragment extends Fragment {
         onlineFriendsStickyAdapter = new OnlineFriendsStickyAdapter(view.getContext(), onlineFriends);
 
         stickyList.setAdapter(onlineFriendsStickyAdapter);
-
+        */
         return view;
     }
 }
