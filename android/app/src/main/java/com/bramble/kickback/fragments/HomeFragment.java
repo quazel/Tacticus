1a2c79fa2395ac76e5c4a039f6ab4ece9ac1ffef
Nathan Fegard <nfegard@asu.edu>
2015-02-04 23:47:17 -0700

    added fragments for use in online button container

--- android/app/src/main/java/com/bramble/kickback/fragments/HomeFragment.java
+++ android/app/src/main/java/com/bramble/kickback/fragments/HomeFragment.java
@@ -14,14 +14,26 @@ public class HomeFragment extends Fragment {
 
     public FragmentManager homeManager;
     public FragmentTransaction homeTransaction;
+    public OfflineFragment offlineFragment;
+    public OnlineFragment onlineFragment;
+    public GoOfflineFragment goOfflineFragment;
+    public CallTextFragment callTextFragment;
+    public TextCancelFragment textCancelFragment;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view =  inflater.inflate(R.layout.fragment_home, container, false);
 
+        offlineFragment = new OfflineFragment();
+        onlineFragment = new OnlineFragment();
+        goOfflineFragment = new GoOfflineFragment();
+        callTextFragment = new CallTextFragment();
+        textCancelFragment = new TextCancelFragment();
+
         homeManager = getFragmentManager();
         homeTransaction = homeManager.beginTransaction();
-        homeTransaction.add(R.id.home_container, new OfflineFragment());
+        homeTransaction.add(R.id.offline_container, offlineFragment);
         homeTransaction.commit();
 
         return view;
@@ -29,13 +41,48 @@ public class HomeFragment extends Fragment {
 
     public void goOnline() {
         homeTransaction = homeManager.beginTransaction();
-        homeTransaction.replace(R.id.home_container, new OnlineFragment());
+        homeTransaction.remove(offlineFragment);
+        homeTransaction.add(R.id.online_container, onlineFragment);
+        homeTransaction.add(R.id.online_button_container, goOfflineFragment);
         homeTransaction.commit();
+        offlineFragment = new OfflineFragment();
     }
 
     public void goOffline() {
         homeTransaction = homeManager.beginTransaction();
-        homeTransaction.replace(R.id.home_container, new OfflineFragment());
+        homeTransaction.remove(onlineFragment);
+        homeTransaction.remove(goOfflineFragment);
+        homeTransaction.add(R.id.offline_container, offlineFragment);
         homeTransaction.commit();
+        onlineFragment = new OnlineFragment();
+        goOfflineFragment = new GoOfflineFragment();
+    }
+
+    public void replaceGoOfflineWithCallText() {
+        homeTransaction = homeManager.beginTransaction();
+        homeTransaction.replace(R.id.online_button_container, callTextFragment);
+        homeTransaction.commit();
+        goOfflineFragment = new GoOfflineFragment();
+    }
+
+    public void replaceCallTextWithTextCancel() {
+        homeTransaction = homeManager.beginTransaction();
+        homeTransaction.replace(R.id.online_button_container, textCancelFragment);
+        homeTransaction.commit();
+        callTextFragment = new CallTextFragment();
+    }
+
+    public void replaceTextCancelWithCallText() {
+        homeTransaction = homeManager.beginTransaction();
+        homeTransaction.replace(R.id.online_button_container, callTextFragment);
+        homeTransaction.commit();
+        textCancelFragment = new TextCancelFragment();
+    }
+
+    public void replaceCallTextWithGoOffline() {
+        homeTransaction = homeManager.beginTransaction();
+        homeTransaction.replace(R.id.online_button_container, goOfflineFragment);
+        homeTransaction.commit();
+        callTextFragment = new CallTextFragment();
     }
 }
