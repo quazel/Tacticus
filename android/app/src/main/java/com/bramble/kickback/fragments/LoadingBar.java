c9ef73e8c1ad82d06da1c7a50ce04e1c629e3ff7
Nathan Fegard <nfegard@asu.edu>
2015-02-06 02:25:40 -0700

    removed useless lines

--- android/app/src/main/java/com/bramble/kickback/fragments/LoadingBar.java
+++ android/app/src/main/java/com/bramble/kickback/fragments/LoadingBar.java
@@ -9,12 +9,10 @@ import android.view.ViewGroup;
 import com.bramble.kickback.R;
 
 public class LoadingBar extends Fragment {
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view =  inflater.inflate(R.layout.fragment_loading, container, false);
         return view;
     }
-
 }
