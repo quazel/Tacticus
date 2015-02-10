432271668159c6e1be4922a18eda219a820e97ec
Nathan Fegard <nfegard@asu.edu>
2015-02-09 23:07:59 -0700

    permissions for camera and photo handler

--- android/app/src/main/java/com/bramble/kickback/fragments/CameraFragment.java
+++ android/app/src/main/java/com/bramble/kickback/fragments/CameraFragment.java
@@ -1,4 +1,18 @@
 package com.bramble.kickback.fragments;
 
-public class CameraFragment {
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bramble.kickback.R;
+
+public class CameraFragment extends Fragment{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view =  inflater.inflate(R.layout.fragment_camera, container, false);
+        return view;
+    }
 }
---
+++ android/app/src/main/java/com/bramble/kickback/handlers/PhotoHandler.java
@@ -0,0 +1,52 @@
+package com.bramble.kickback.handlers;
+
+import android.content.Context;
+import android.hardware.Camera;
+import android.os.Environment;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class PhotoHandler implements Camera.PictureCallback {
+
+    private final Context context;
+
+    public PhotoHandler(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public void onPictureTaken(byte[] data, Camera camera) {
+
+        File pictureFileDir = getDir();
+
+        if (!pictureFileDir.exists() && !pictureFileDir.mkdirs()) {
+            return;
+        }
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyymmddhhmmss");
+        String date = dateFormat.format(new Date());
+        String photoFile = "Picture_" + date + ".jpg";
+
+        String filename = pictureFileDir.getPath() + File.separator + photoFile;
+
+        File pictureFile = new File(filename);
+
+        try {
+            FileOutputStream fos = new FileOutputStream(pictureFile);
+            fos.write(data);
+            fos.close();
+        }
+        catch (Exception error) {
+
+        }
+    }
+
+    private File getDir() {
+        File sdDir = Environment
+                .getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        return new File(sdDir, "Kickback");
+    }
+}
