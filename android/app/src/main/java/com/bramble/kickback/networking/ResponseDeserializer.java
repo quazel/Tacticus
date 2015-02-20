f8a138a61ef094138983117b7e23f18b5e96caef
Steve King, Jr <stevekx86@gmail.com>
2015-02-19 18:13:17 -0700

    Added a deserialize method that updates information on a poll.

--- android/app/src/main/java/com/bramble/kickback/networking/ResponseDeserializer.java
+++ android/app/src/main/java/com/bramble/kickback/networking/ResponseDeserializer.java
@@ -1,7 +1,35 @@
 package com.bramble.kickback.networking;
 
+import com.bramble.kickback.models.Friend;
+import com.bramble.kickback.models.User;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class ResponseDeserializer {
 
-    public static void parsePoll()
+    public static void deserializePoll(String response) throws JSONException {
+        User theUser = User.getUser();
+        theUser.getFriends().clear();
+
+        JSONObject jsonObject = new JSONObject(response);
+        String sessionID = jsonObject.getString("session_id");
+        int callMe = jsonObject.getInt("call_me");
+        JSONArray friendsJSON = jsonObject.getJSONArray("friends");
+
+        for (int i = 0; i < friendsJSON.length(); i++) {
+            JSONObject friendJSON = friendsJSON.getJSONObject(i);
+            String email = friendJSON.getString("email");
+            String nickname = friendJSON.getString("nickname");
+            String name = friendJSON.getString("name");
+            String phoneNumber = friendJSON.getString("phone_number");
+            boolean online = friendJSON.getBoolean("online");
+
+            Friend friend = new Friend(nickname, name, phoneNumber);
+            friend.setOnline(online);
+            theUser.addFriend(friend);
+        }
+    }
 
 }
