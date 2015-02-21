64761ae0d56481ca7c3b33bdc42a3dab29fecd2e
Nathan Fegard <nfegard@asu.edu>
2015-02-21 02:32:37 -0700

    finished most methods in add friends search adapter and added it to the search activity

--- android/app/src/main/java/com/bramble/kickback/activities/AddFriendsSearch.java
+++ android/app/src/main/java/com/bramble/kickback/activities/AddFriendsSearch.java
@@ -7,23 +7,33 @@ import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.bramble.kickback.R;
+import com.bramble.kickback.adapters.AddFriendSearchResultsAdapter;
+import com.bramble.kickback.models.RemoteUser;
+
+import java.util.ArrayList;
 
 public class AddFriendsSearch extends Activity {
 
     private EditText phoneNumberEdittext;
+    private ListView resultsList;
+    private AddFriendSearchResultsAdapter resultsAdapter;
+    private ArrayList<RemoteUser> remoteUsers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_friends_search);
 
+        resultsList = (ListView) findViewById(R.id.search_results_list);
+        resultsAdapter = new AddFriendSearchResultsAdapter(this, remoteUsers);
+        resultsList.setAdapter(resultsAdapter);
+
         phoneNumberEdittext = (EditText) findViewById(R.id.search_edittext);
-
         phoneNumberEdittext.requestFocus();
-
         phoneNumberEdittext.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
                 if ((event != null && (event.getKeyCode() == KeyEvent.KEYCODE_ENTER)) || (actionId == EditorInfo.IME_ACTION_DONE)) {
--- android/app/src/main/java/com/bramble/kickback/adapters/AddFriendSearchResultsAdapter.java
+++ android/app/src/main/java/com/bramble/kickback/adapters/AddFriendSearchResultsAdapter.java
@@ -1,4 +1,58 @@
 package com.bramble.kickback.adapters;
 
-public class AddFriendSearchResultsAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.bramble.kickback.R;
+import com.bramble.kickback.models.RemoteUser;
+
+import java.util.ArrayList;
+
+public class AddFriendSearchResultsAdapter extends BaseAdapter {
+
+    private ArrayList<RemoteUser> remoteUsers;
+    private LayoutInflater layoutInflater;
+    private Context context;
+
+    public AddFriendSearchResultsAdapter(Context context, ArrayList<RemoteUser> remoteUsers) {
+        this.context = context;
+        this.remoteUsers = remoteUsers;
+        layoutInflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public int getCount() {
+        return remoteUsers.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return remoteUsers.get(position);
+    }
+
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    public class ViewHolder {
+
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder = new ViewHolder();
+        RemoteUser remoteUser = remoteUsers.get(position);
+        convertView = layoutInflater.inflate(R.layout.friends_list_item, parent, false);
+
+        return convertView;
+    }
+
 }
