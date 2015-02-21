baf864a40171f845f7a7f770fd953a0175dec3f6
Nathan Fegard <nfegard@asu.edu>
2015-02-20 18:10:43 -0700

    search list view added and ui polished

--- android/app/src/main/java/com/bramble/kickback/activities/AddFriendsSearch.java
+++ android/app/src/main/java/com/bramble/kickback/activities/AddFriendsSearch.java
@@ -3,8 +3,11 @@ package com.bramble.kickback.activities;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.bramble.kickback.R;
 
@@ -18,15 +21,25 @@ public class AddFriendsSearch extends Activity {
         setContentView(R.layout.activity_add_friends_search);
 
         phoneNumberEdittext = (EditText) findViewById(R.id.search_edittext);
+
+        phoneNumberEdittext.requestFocus();
+
+        phoneNumberEdittext.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if ((event != null && (event.getKeyCode() == KeyEvent.KEYCODE_ENTER)) || (actionId == EditorInfo.IME_ACTION_DONE)) {
+                    //search();
+                }
+                return false;
+            }
+        });
     }
 
     public void searchBackButtonPressed(View view) {
-        Intent intent = new Intent(AddFriendsSearch.this, Main.class);
-        startActivity(intent);
         finish();
     }
 
     public void searchPhoneNumberPressed(View view) {
-
+        //search();
     }
+
 }
