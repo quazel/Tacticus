4d1bf72ae0bdd820a3d7cc0d14c13e0395b7f1ee
Nathan Fegard <nfegard@asu.edu>
2015-02-21 15:37:58 -0700

    completed adapter for search results

--- android/app/src/main/java/com/bramble/kickback/adapters/AddFriendSearchResultsAdapter.java
+++ android/app/src/main/java/com/bramble/kickback/adapters/AddFriendSearchResultsAdapter.java
@@ -5,13 +5,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bramble.kickback.R;
 import com.bramble.kickback.models.RemoteUser;
 
 import java.util.ArrayList;
 
-public class AddFriendSearchResultsAdapter extends BaseAdapter {
+import se.emilsjolander.stickylistheaders.StickyListHeadersAdapter;
+
+public class AddFriendSearchResultsAdapter extends BaseAdapter implements StickyListHeadersAdapter {
 
     private ArrayList<RemoteUser> remoteUsers;
     private LayoutInflater layoutInflater;
@@ -40,16 +45,97 @@ public class AddFriendSearchResultsAdapter extends BaseAdapter {
     }
 
     public class ViewHolder {
-
+        TextView searchNickname;
+        TextView searchPhoneNumber;
+        Button addButton;
+        Button removeButton;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         ViewHolder holder = new ViewHolder();
         RemoteUser remoteUser = remoteUsers.get(position);
-        convertView = layoutInflater.inflate(R.layout.add_search_result_item, parent, false);
+        if(remoteUsers.get(position).isFriend()) {
+            convertView = layoutInflater.inflate(R.layout.remove_search_result_item, parent, false);
+            holder.removeButton = (Button) convertView.findViewById(R.id.search_remove_button);
+            holder.searchNickname = (TextView) convertView.findViewById(R.id.search_remove_nickname_thing);
+            holder.searchPhoneNumber = (TextView) convertView.findViewById(R.id.search_remove_phone_number_thing);
+
+            holder.removeButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View arg0) {
+                    Toast.makeText(context, "remove button pressed", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+        else {
+            convertView = layoutInflater.inflate(R.layout.add_search_result_item, parent, false);
+            holder.addButton = (Button) convertView.findViewById(R.id.search_add_button);
+            holder.searchNickname = (TextView) convertView.findViewById(R.id.search_add_nickname_thing);
+            holder.searchPhoneNumber = (TextView) convertView.findViewById(R.id.search_add_phone_number_thing);
+
+            holder.addButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View arg0) {
+                    Toast.makeText(context,"add button pressed", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+        holder.searchNickname.setText(remoteUsers.get(position).getNickname());
+        holder.searchPhoneNumber.setText(remoteUsers.get(position).getPhoneNumber());
 
         return convertView;
     }
 
+    @Override
+    public View getHeaderView(int position, View convertView, ViewGroup parent) {
+        HeaderViewHolder holder;
+        if (convertView == null) {
+            holder = new HeaderViewHolder();
+            convertView = layoutInflater.inflate(R.layout.header_sticky, parent, false);
+            holder.text = (TextView) convertView.findViewById(R.id.text);
+            convertView.setTag(holder);
+        }
+        else {
+            holder = (HeaderViewHolder) convertView.getTag();
+        }
+        String headerText;
+        if(remoteUsers.get(position).isFriend()) {
+            headerText = "FRIENDS";
+        }
+        else {
+            headerText = "USERS";
+        }
+        /*
+        else if (!filteredList.get(position).isFavorite() && !filteredList.get(position).isHidden()){
+            headerText = "FRIENDS";
+        }
+        else {
+            headerText = "HIDDEN";
+        } */
+        holder.text.setText(headerText);
+        return convertView;
+    }
+
+    @Override
+    public long getHeaderId(int position) {
+        if(remoteUsers.get(position).isFriend()) {
+            return 0;
+        }
+        else {
+            return 1;
+        }
+        /*
+        else if (!filteredList.get(position).isFavorite() && !filteredList.get(position).isHidden()) {
+            return 1;
+        }
+        else {
+            return 2;
+        }
+        */
+    }
+
+    class HeaderViewHolder {
+        TextView text;
+    }
 }
