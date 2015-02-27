cb6a4dee92f568ae5223337d57a1b0a1d14603a8
Steve King, Jr <stevekx86@gmail.com>
2015-02-27 14:04:15 -0700

    Began work on a ContactLayer abstraction for reading, creating, and updating contacts.

--- android/app/src/main/java/com/bramble/kickback/contacts/ContactLayer.java
+++ android/app/src/main/java/com/bramble/kickback/contacts/ContactLayer.java
@@ -1,30 +1,33 @@
-package com.bramble.kickback.util;
+package com.bramble.kickback.contacts;
 
 import android.content.ContentResolver;
 import android.database.Cursor;
 import android.provider.ContactsContract;
 
+import com.bramble.kickback.models.Friend;
 import com.bramble.kickback.models.Person;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class Globals {
+public class ContactLayer {
 
-    public static ArrayList<Person> contacts = new ArrayList<Person>();
+    private ContentResolver mContentResolver;
+    private List<Person> contacts;
 
 
-    //needs work
-    public static void readContacts(ContentResolver cr) {
-        Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI,
+    public ContactLayer(ContentResolver cr) {
+        mContentResolver = cr;
+        contacts = new ArrayList<Person>();
+        Cursor cur = mContentResolver.query(ContactsContract.Contacts.CONTENT_URI,
                 null, null, null, null);
-
         if (cur.getCount() > 0) {
             while (cur.moveToNext()) {
                 String id = cur.getString(cur.getColumnIndex(ContactsContract.Contacts._ID));
                 String name = cur.getString(cur.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
                 if (Integer.parseInt(cur.getString(cur.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER))) > 0) {
                     // get the phone number
-                    Cursor pCur = cr.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,null,
+                    Cursor pCur = mContentResolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,null,
                             ContactsContract.CommonDataKinds.Phone.CONTACT_ID +" = ?",
                             new String[]{id}, null);
                     ArrayList<String> phoneNumbers = new ArrayList<String>();
@@ -38,6 +41,8 @@ public class Globals {
                 }
             }
         }
+        cur.close();
     }
+
 
 }
