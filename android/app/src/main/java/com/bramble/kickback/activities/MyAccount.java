84e9d25d653f28b38a2c1f7f34368feca50e9626
Nathan Fegard <nfegard@asu.edu>
2015-02-26 17:16:01 -0700

    all things

--- android/app/src/main/java/com/bramble/kickback/activities/MyAccount.java
+++ android/app/src/main/java/com/bramble/kickback/activities/MyAccount.java
@@ -1,9 +1,13 @@
 package com.bramble.kickback.activities;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.bramble.kickback.R;
@@ -29,6 +33,41 @@ public class MyAccount extends Activity {
         nickname.setText(User.getUser().getNickname());
         phoneNumber.setText(Util.formatPhoneNumber(User.getUser().getPhoneNumber()));
         email.setText(User.getUser().getEmail());
+    }
+
+    public void myAccountPhonePressed(View view) {
+
+    }
+
+    public void myAccountEmailPressed(View view) {
+        AlertDialog.Builder builder = new   AlertDialog.Builder(MyAccount.this);
+        AlertDialog alertDialog = builder.create();
+        alertDialog.setTitle("Email");
+        alertDialog.setMessage("Your email address is used to log in and  recover your account." +
+                               " Please make sure your email is correct before confirming.");
+
+        final EditText input = new EditText(this);
+        input.setInputType(InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS);
+        input.setText(User.getUser().getEmail());
+        alertDialog.setView(input);
+        alertDialog.setButton(DialogInterface.BUTTON_POSITIVE, "Confirm",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,
+                                        int whichButton) {
+                        // code that checks to see if the email is valid or used by another account
+                        // if it passes checks make it the new email and close alert dialog
+
+                    }
+
+                });
+
+        alertDialog.setButton(DialogInterface.BUTTON_NEGATIVE, "Cancel",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,
+                                        int whichButton) {
+                    }
+                });
+        alertDialog.show();
     }
 
     public void myAccountBackButtonPressed(View view) {
