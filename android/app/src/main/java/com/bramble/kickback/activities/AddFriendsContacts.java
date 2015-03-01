aee4942609c77ec1fd892fc6bd73631137463fc0
Steve King, Jr <stevekx86@gmail.com>
2015-02-28 21:48:55 -0700

    We have liftoff.

--- android/app/src/main/java/com/bramble/kickback/activities/AddFriendsContacts.java
+++ android/app/src/main/java/com/bramble/kickback/activities/AddFriendsContacts.java
@@ -6,12 +6,16 @@ import android.app.FragmentTransaction;
 import android.content.pm.ActivityInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.bramble.kickback.R;
+import com.bramble.kickback.adapters.AddContactsAdapter;
+import com.bramble.kickback.adapters.InviteContactsAdapter;
 import com.bramble.kickback.fragments.LoadingBar;
 import com.bramble.kickback.contacts.ContactLayer;
+import com.bramble.kickback.models.Person;
+import com.bramble.kickback.models.RemoteUser;
 import com.bramble.kickback.models.User;
 import com.bramble.kickback.networking.ConnectionHandler;
 import com.bramble.kickback.networking.ContactPayloadSerializer;
@@ -19,18 +23,37 @@ import com.bramble.kickback.networking.ContactPayloadSerializer;
 import org.json.JSONException;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import se.emilsjolander.stickylistheaders.StickyListHeadersListView;
 
 public class AddFriendsContacts extends Activity {
 
     private LoadingBar loadingBar;
     private FragmentManager fm;
     private FragmentTransaction ft;
+    private ArrayList<Person> uninvitedPeople;
+    private ArrayList<RemoteUser> invitedPeople;
+    private InviteContactsAdapter mInviteContactsAdapter;
+    private AddContactsAdapter mAddContactsAdapter;
+    private StickyListHeadersListView mInviteList;
+    private StickyListHeadersListView mAddList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_friends_contacts);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        uninvitedPeople = new ArrayList<Person>();
+        invitedPeople = new ArrayList<RemoteUser>();
+        mInviteContactsAdapter = new InviteContactsAdapter(this, uninvitedPeople);
+        mAddContactsAdapter = new AddContactsAdapter(this, invitedPeople);
+        mInviteList = (StickyListHeadersListView) findViewById(R.id.contacts_without_accounts_list);
+        mAddList = (StickyListHeadersListView) findViewById(R.id.contacts_with_accounts_list);
+        mInviteList.setAdapter(mInviteContactsAdapter);
+        mAddList.setAdapter(mAddContactsAdapter);
 
         loadingBar = new LoadingBar();
 
@@ -53,9 +76,28 @@ public class AddFriendsContacts extends Activity {
             try {
                 ContactLayer contactLayer = ContactLayer.getInstance();
                 ContactPayloadSerializer contactPayloadSerializer = new ContactPayloadSerializer(contactLayer.getContacts());
-                String jsonString = contactPayloadSerializer.serialize();
+
+                String jsonString = contactPayloadSerializer.serializePayload();
                 String response = new ConnectionHandler().checkContacts(User.getUser().getSessionId(), jsonString);
-                return response.startsWith("200:");
+
+                if (response.startsWith("200:")) {
+                    response = response.replace("200:", "");
+
+                    invitedPeople.addAll(contactPayloadSerializer.deserializeResponse(response));
+
+                    HashMap<String, Person> contactsBuffer = new HashMap<String, Person>(contactLayer.getContacts());
+                    for (RemoteUser remoteUser : invitedPeople) {
+                        if (contactsBuffer.containsKey(remoteUser.getPhoneNumber())) {
+                            contactsBuffer.remove(remoteUser.getPhoneNumber());
+                        }
+                    }
+                    uninvitedPeople.addAll(contactsBuffer.values());
+
+                    return true;
+                }
+                else {
+                    return false;
+                }
             } catch (IOException e) {
                 e.printStackTrace();
                 return false;
@@ -67,9 +109,19 @@ public class AddFriendsContacts extends Activity {
 
         @Override
         protected void onPostExecute(Boolean result) {
-            ft = fm.beginTransaction();
-            ft.remove(loadingBar);
-            ft.commit();
+            if (result) {
+                ft = fm.beginTransaction();
+                ft.remove(loadingBar);
+                ft.commit();
+
+                mInviteContactsAdapter.notifyDataSetChanged();
+                mAddContactsAdapter.notifyDataSetChanged();
+                mInviteList.invalidateViews();
+                mAddList.invalidateViews();
+            }
+            else {
+                Toast.makeText(AddFriendsContacts.this, "Failed to retrieve contacts.", Toast.LENGTH_LONG).show();
+            }
         }
 
     }
--- android/app/src/main/java/com/bramble/kickback/networking/ContactPayloadSerializer.java
+++ android/app/src/main/java/com/bramble/kickback/networking/ContactPayloadSerializer.java
@@ -1,6 +1,7 @@
 package com.bramble.kickback.networking;
 
 import com.bramble.kickback.models.Person;
+import com.bramble.kickback.models.RemoteUser;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -17,7 +18,7 @@ public class ContactPayloadSerializer {
         this.contacts = contacts;
     }
 
-    public String serialize() throws JSONException {
+    public String serializePayload() throws JSONException {
         JSONObject jsonObject = new JSONObject();
         JSONArray jsonArray = new JSONArray();
 
@@ -29,8 +30,22 @@ public class ContactPayloadSerializer {
         return jsonObject.toString();
     }
 
-    public ArrayList<Person> deserialize(String jsonString) {
-        return null;
+    public ArrayList<RemoteUser> deserializeResponse(String jsonString) throws JSONException {
+        JSONArray jsonArray = new JSONArray(jsonString);
+        ArrayList<RemoteUser> people = new ArrayList<RemoteUser>();
+        for (int i = 0; i < jsonArray.length(); i++) {
+            JSONObject jsonObject = jsonArray.getJSONObject(i);
+            String name = jsonObject.getString("name");
+            String nickname = jsonObject.getString("nickname");
+            String email = jsonObject.getString("email");
+            String phoneNumber = jsonObject.getString("phone_number");
+            String sex = jsonObject.getString("sex");
+
+            RemoteUser remoteUser = new RemoteUser(nickname, name, email, phoneNumber, sex, false, true);
+            people.add(remoteUser);
+        }
+
+        return people;
     }
 
 }
