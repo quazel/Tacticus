facb00b210b4be8a38a2b0d88e6fdf4374185314
Steve King, Jr <stevekx86@gmail.com>
2015-03-02 11:01:07 -0700

    Initialize ContactLayer on startup.

--- android/app/src/main/java/com/bramble/kickback/activities/SplashScreen.java
+++ android/app/src/main/java/com/bramble/kickback/activities/SplashScreen.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.os.Handler;
 
 import com.bramble.kickback.R;
+import com.bramble.kickback.contacts.ContactLayer;
 import com.bramble.kickback.models.User;
 import com.crashlytics.android.Crashlytics;
 
@@ -23,6 +24,7 @@ public class SplashScreen extends Activity {
         Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_splash_screen);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        ContactLayer.initialize(getContentResolver());
         if (!User.getUser().getNickname().equals("")) {
             new Handler().postDelayed(new Runnable() {
                 @Override
