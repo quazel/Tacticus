c078f8bbe079d1993126f38deb51c7fd35f7c41e
Steve King, Jr <stevekx86@gmail.com>
2015-03-11 18:27:49 -0700

    Moved methods to the UserDatabaseHelper class; seemed more appropriate.

--- android/app/src/main/java/com/bramble/kickback/data/UserDatabaseHelper.java
+++ android/app/src/main/java/com/bramble/kickback/data/UserDatabaseHelper.java
@@ -1,6 +1,8 @@
 package com.bramble.kickback.data;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -26,6 +28,61 @@ public class UserDatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         UserDataTable.onUpgrade(db, oldVersion, newVersion);
+    }
+
+    public void setNotificationOnFriendRequest(SQLiteDatabase database, boolean flag) {
+        String[] projection = {
+                UserDataTable.COLUMN_ID,
+        };
+        Cursor c = database.query(UserDataTable.TABLE_USER,
+                projection,
+                null,
+                null,
+                null,
+                null,
+                null);
+        c.moveToFirst();
+        if (c.getCount() > 0) {
+            int rowID = c.getInt(c.getColumnIndexOrThrow(UserDataTable.COLUMN_ID));
+            ContentValues contentValues = new ContentValues();
+            contentValues.put(UserDataTable.COLUMN_NOTIFY_FRIEND_REQUEST, flag);
+            String selection = UserDataTable.COLUMN_ID + " LIKE ?";
+            String[] selectionArgs = { String.valueOf(rowID) };
+            int count = database.update(UserDataTable.TABLE_USER,
+                    contentValues,
+                    selection,
+                    selectionArgs);
+        }
+        c.close();
+    }
+
+    // 0: Any friends Kickback
+    // 1: Favorites Kickback
+    // 2: Never
+    public void setNotificationOnFriendActivity(SQLiteDatabase database, int flag) {
+        String[] projection = {
+                UserDataTable.COLUMN_ID,
+        };
+        Cursor c = database.query(UserDataTable.TABLE_USER,
+                projection,
+                null,
+                null,
+                null,
+                null,
+                null);
+        c.moveToFirst();
+        if (c.getCount() > 0) {
+            int rowID = c.getInt(c.getColumnIndexOrThrow(UserDataTable.COLUMN_ID));
+            ContentValues contentValues = new ContentValues();
+            contentValues.put(UserDataTable.COLUMN_NOTIFY_FRIEND_ACTIVITY, flag);
+            String selection = UserDataTable.COLUMN_ID + " LIKE ?";
+            String[] selectionArgs = { String.valueOf(rowID) };
+            int count = database.update(UserDataTable.TABLE_USER,
+                    contentValues,
+                    selection,
+                    selectionArgs);
+        }
+        c.close();
     }
 
 }
