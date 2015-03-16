0e8672ca461f93dfe3cdf053610a3e0d00375feb
Steve King, Jr <stevekx86@gmail.com>
2015-03-16 11:42:51 -0700

    New Idea: Caches and Layers.

---
+++ android/app/src/main/java/com/bramble/kickback/data/SettingsLayer.java
@@ -0,0 +1,11 @@
+package com.bramble.kickback.data;
+
+/*
+ * Conceptual work on a cache for settings
+ * Changes would only be read and written by necessity, but data about settings remains cached in memory
+ */
+public class SettingsLayer {
+
+    
+
+}
