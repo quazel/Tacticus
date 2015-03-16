2fa124b3f48cd5586abcf2f2abef90673cf43320
Steve King, Jr <stevekx86@gmail.com>
2015-03-15 21:02:55 -0700

    Methods for accessing notification settings.

--- android/app/src/main/java/com/bramble/kickback/data/UserPreferencesLayer.java
+++ android/app/src/main/java/com/bramble/kickback/data/UserPreferencesLayer.java
@@ -8,6 +8,11 @@ public class UserPreferencesLayer {
 
     private SharedPreferences preferences;
 
+    private final String NOTIFICATION_ON_FRIEND_REQUEST = "friend_request_notification";
+    private final String NOTICIATION_ON_FRIEND_ACTIVITY = "friend_activity_notification";
+    private final String NOTIFIERS = "notifiers";
+
+
     private UserPreferencesLayer(SharedPreferences preferences) {
         this.preferences = preferences;
     }
@@ -23,6 +28,50 @@ public class UserPreferencesLayer {
             throw new IllegalStateException("UserPreferencesLayer not initialized!");
         }
         return instance;
+    }
+
+    public void setNotificationOnFriendRequest(boolean flag) {
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putBoolean(NOTIFICATION_ON_FRIEND_REQUEST, flag);
+        editor.apply();
+    }
+
+    // 0: Any friends Kickback
+    // 1: Favorites Kickback
+    // 2: Never
+    public void setNotificationOnFriendActivity(int flag) {
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putInt(NOTICIATION_ON_FRIEND_ACTIVITY, flag);
+        editor.apply();
+    }
+
+    // Flag values:
+    // 0b001: LED blinking
+    // 0b010: Vibration
+    // 0b100: Sound
+    public void setNotifiers(int flag) {
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putInt(NOTIFIERS, flag);
+        editor.apply();
+    }
+
+    public boolean getNotificationOnFriendRequest() {
+        return preferences.getBoolean(NOTIFICATION_ON_FRIEND_REQUEST, false);
+    }
+
+    // 0: Any friends Kickback
+    // 1: Favorites Kickback
+    // 2: Never
+    public int getNoticationOnFriendActivity() {
+        return preferences.getInt(NOTICIATION_ON_FRIEND_ACTIVITY, 1);
+    }
+
+    // Flag values:
+    // 0b001: LED blinking
+    // 0b010: Vibration
+    // 0b100: Sound
+    public int getNotifiers() {
+        return preferences.getInt(NOTIFIERS, 0b101);
     }
 
 }
