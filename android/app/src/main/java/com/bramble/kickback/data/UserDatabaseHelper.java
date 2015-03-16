b0927b71b0fa4fdccda2a43c8ef14a438ff0b20c
Steve King, Jr <stevekx86@gmail.com>
2015-03-16 10:36:52 -0700

    Notification settings getters.

--- android/app/src/main/java/com/bramble/kickback/data/UserDatabaseHelper.java
+++ android/app/src/main/java/com/bramble/kickback/data/UserDatabaseHelper.java
@@ -31,6 +31,7 @@ public class UserDatabaseHelper extends SQLiteOpenHelper {
     }
 
     public void setNotificationOnFriendRequest(SQLiteDatabase database, boolean flag) {
+        int writeFlag = flag ? 1 : 0;
         String[] projection = {
                 UserDataTable.COLUMN_ID,
         };
@@ -45,7 +46,7 @@ public class UserDatabaseHelper extends SQLiteOpenHelper {
         if (c.getCount() > 0) {
             int rowID = c.getInt(c.getColumnIndexOrThrow(UserDataTable.COLUMN_ID));
             ContentValues contentValues = new ContentValues();
-            contentValues.put(UserDataTable.COLUMN_NOTIFY_FRIEND_REQUEST, flag);
+            contentValues.put(UserDataTable.COLUMN_NOTIFY_FRIEND_REQUEST, writeFlag);
             String selection = UserDataTable.COLUMN_ID + " LIKE ?";
             String[] selectionArgs = { String.valueOf(rowID) };
             int count = database.update(UserDataTable.TABLE_USER,
@@ -113,6 +114,66 @@ public class UserDatabaseHelper extends SQLiteOpenHelper {
                     selectionArgs);
         }
         c.close();
+    }
+
+    public boolean getNotificationOnFriendRequest(SQLiteDatabase database) {
+        String[] projection = {
+                UserDataTable.COLUMN_ID,
+        };
+        Cursor c = database.query(UserDataTable.TABLE_USER,
+                projection,
+                null,
+                null,
+                null,
+                null,
+                null);
+        c.moveToFirst();
+        boolean flag = false;
+        if (c.getCount() > 0) {
+            flag = c.getInt(c.getColumnIndexOrThrow(UserDataTable.COLUMN_NOTIFY_FRIEND_REQUEST)) > 0;
+        }
+        c.close();
+        return flag;
+    }
+
+    public int getNotificationOnFriendActivity(SQLiteDatabase database) {
+        String[] projection = {
+                UserDataTable.COLUMN_ID,
+        };
+        Cursor c = database.query(UserDataTable.TABLE_USER,
+                projection,
+                null,
+                null,
+                null,
+                null,
+                null);
+        c.moveToFirst();
+        int flag = 0;
+        if (c.getCount() > 0) {
+            flag = c.getInt(c.getColumnIndexOrThrow(UserDataTable.COLUMN_NOTIFY_FRIEND_ACTIVITY));
+        }
+        c.close();
+        return flag;
+    }
+
+    public int getNotifiers(SQLiteDatabase database) {
+        String[] projection = {
+                UserDataTable.COLUMN_ID,
+        };
+        Cursor c = database.query(UserDataTable.TABLE_USER,
+                projection,
+                null,
+                null,
+                null,
+                null,
+                null);
+        c.moveToFirst();
+        int flag = 0;
+        if (c.getCount() > 0) {
+            flag = c.getInt(c.getColumnIndexOrThrow(UserDataTable.COLUMN_NOTIFIERS));
+        }
+        c.close();
+        return flag;
     }
 
 }
